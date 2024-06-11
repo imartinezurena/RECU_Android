3b9d9c8a9a48264ecd5d1253479e6b90761b4cd3
Imartinezurena <nachomartinezurena@gmail.com>
2024-06-11 20:58:42 +0200

    avance RyM

--- RyM/app/src/main/java/com/example/recuperacionfinal/EjemploRyM/AdapterRyM.java
+++ RyM/app/src/main/java/com/example/recuperacionfinal/EjemploRyM/AdapterRyM.java
@@ -3,6 +3,7 @@ package com.example.recuperacionfinal.EjemploRyM;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
@@ -13,33 +14,62 @@ import com.example.recuperacionfinal.R;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AdapterRyM extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class AdapterRyM extends RecyclerView.Adapter<AdapterRyM.ViewHolder> {
     private List<PersonajeRyM> personajes;
+
     public AdapterRyM() {
         personajes = new ArrayList<>();
     }
+
     public interface ItemClickListener {
         void onClick(View view, int position, PersonajeRyM personaje);
     }
+
     private ItemClickListener clickListener;
 
     public void setClickListener(ItemClickListener itemClickListener) {
         this.clickListener = itemClickListener;
     }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private final TextView tvNombre;
+
+
+        public ViewHolder(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+
+            tvNombre = (TextView) view.findViewById(R.id.nombreDetallerPersonaje);
+            view.setOnClickListener(this);
+        }
+
+        public void setName(String nombre) {
+            tvNombre.setText(nombre);
+
+        }
+
+        @Override
+        public void onClick(View view) {
+            // Si tengo un manejador de evento lo propago con el índice
+            if (clickListener != null)
+                clickListener.onClick(view, getAdapterPosition(), personajes.get(getAdapterPosition()));
+        }
+    }
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.fila_personajes, parent, false);
 
-        return new RecyclerView.ViewHolder(view);
+        return new ViewHolder(view);
         //return null;
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         PersonajeRyM personaje = personajes.get(position);
-        holder.setInfo(personaje.getName());
+        holder.setName(personaje.getName());
     }
 
     @Override
